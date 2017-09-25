7833e1e0c48b3e8b22d7da2f0cd016b872621bc3
Jesper Rindom Jensen <jrj@create.aau.dk>
2017-09-25 15:32:10 +0200

    added some content to the tree example lecture 5

--- Lecture5/BinaryTreeExample/main.cpp
+++ Lecture5/BinaryTreeExample/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+struct TreeNode{
+    int data;
+    TreeNode *left_link;
+    TreeNode *right_link;
+};
+typedef TreeNode* TreeNodePtr;
+
+void add_leaf(TreeNodePtr parent_ptr, int the_number);
+
 int main()
 {
     cout << "Hello world!" << endl;
     return 0;
+}
+
+void add_leaf(TreeNodePtr root_ptr, int the_number){
+    if(root_ptr->left_link==NULL&&root_ptr->right_link==NULL){
+        root_ptr
+    }
 }
